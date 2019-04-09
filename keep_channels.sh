b30611c65d506fbd48b4ed4828086be84c4a8beb
Payam Nab <papampi@users.noreply.github.com>
2019-04-09 14:33:31 +0430

    Update keep_channels.sh

--- keep_channels.sh
+++ keep_channels.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-wget -O /home/$USER/Downloads/tv_channels_plus.m3u "http://sampleiptv.com:1557/get.php?username=xxxxxx&password=xxxxxxx&type=m3u_plus&output=ts"
+cd /home/$USER/Downloads/
+wget -O /home/$USER/Downloads/tv_channels.m3u "http://sampleiptv.com:1557/get.php?username=xxxxxx&password=xxxxxxx&type=m3u_plus&output=ts"
 m3ulist="/home/$USER/Downloads/tv_channels.m3u"
 channel_list="/home/$USER/Downloads/tv_channels_plus.m3u"
 rm $channel_list
